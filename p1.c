3534259dce7684281809c738d5697c386b0936a0
akirakiraro <rodrigoakirakira@gmail.com>
2024-09-06 16:30:41 -0300

    comeco_v0.2
    Mexi em algumas coisas e principalmente eu escrevi oq cada coisa faz

--- p1.c
+++ p1.c
@@ -3,45 +3,60 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int add_usuario (int cpf, int senha){
-
-    FILE *login;
-    login = fopen ("usuario.bin", "wb");
-
-    fwrite(&cpf, sizeof(int), 1, login);
-    fwrite(&senha, sizeof(int), 1, login);
-    fclose(login);
+void limpa(){
+    system("cls");                                              // da um clear no terminal
 }
 
-int Login_cpf (int cpf_entrada){
-    int cpf_usuario;
+// funcao pra futuramente add um usuario e tem que colocar no maximo 10 usuarios
+int add_usuario (int cpf, int senha){
 
-    FILE *login;
-    login = fopen ("usuario.bin", "rb");
-    fread(&cpf_usuario, sizeof(cpf_usuario), 1, login);
-    fclose(login);
-    if (cpf_entrada == cpf_usuario){
+    FILE *login;                                                // define que login  eh um arquivo
+    login = fopen ("usuario.bin", "wb");                        // abre o arquivo em modo de "write binario"
+    fwrite(&cpf, sizeof(int), 1, login);                        // escreve no arquivo o cpf
+    fwrite(&senha, sizeof(int), 1, login);                      // escreve no arquivo a senha
+    fclose(login);                                              // fecha o arquivo
+}
+
+int Login_cpf (){                                               // Funcao que faz o login do usuario -------------------------------------------------------------
+
+    int cpf_entrada;                                            // define um int para cpf_entrada
+    int aprovacao_cpf = 0;
+    int *ptr_aprovacao; 
+    ptr_aprovacao = &aprovacao_cpf;
+    printf("%d", *ptr_aprovacao);
+    while (*ptr_aprovacao == 0){
+        limpa();
+        printf("Digite seu cpf: \n");                               // fala pra ele digitar o cpf_entrada
+        scanf("%d", &cpf_entrada);                                  // pega o cpf que ele digitar
+    }
+    
+    limpa();
+
+    int cpf_usuario;                                            // define um cpf_usuario
+    FILE *login;                                                // define que login eh arquivo
+    login = fopen ("usuario.bin", "rb");                        // abri o arquivo em read binario
+    fread(&cpf_usuario, sizeof(cpf_usuario), 1, login);         // le o arquivo, armazena na variavel "&cpf_usuario"
+    fclose(login);                                              // fecha o arquivo
+    if (cpf_entrada == cpf_usuario){                            // verifica se o cpf que ele digitou for igual ao cpf que tem no arquivo
         printf("Verdade\n");
+        *ptr_aprovacao = 1;
     }else{
         printf("Falso\n");
     }
 
 }
 
-int main(){
 
+int main(){
     int cpf = 123;
     int senha = 321;
     add_usuario(cpf, senha);
 
-    int cpf_entrada;
+    // Comeca os codigos certos a partir daqui.
 
-    printf("Digite seu cpf: \n");
-    scanf("%d", &cpf_entrada);
+    Login_cpf();
 
-    Login_cpf(cpf_entrada);
-
-    printf("entrada: %d\n", cpf_entrada);
+    printf("entrou!!\n");
 
     return 0;
 }
