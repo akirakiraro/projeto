17ba4acdddc6d859fe05dc81271167768cb7a3ac
akirakiraro <rodrigoakirakira@gmail.com>
2024-09-05 23:05:10 -0300

    Comeco_v0.1
    Comecei a fazer o login de cpf, mas fiquei cansado e fui dormir.

--- p1.c
+++ p1.c
@@ -3,46 +3,45 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int add_usuario (int *cpf, int *senha){
+int add_usuario (int cpf, int senha){
 
     FILE *login;
     login = fopen ("usuario.bin", "wb");
 
-    fwrite(cpf, sizeof(int), 1, login);
-    fwrite(senha, sizeof(int), 1, login);
+    fwrite(&cpf, sizeof(int), 1, login);
+    fwrite(&senha, sizeof(int), 1, login);
     fclose(login);
 }
 
-int le_usuario (int *cpf, int *senha){
-    int i = 0;
+int Login_cpf (int cpf_entrada){
+    int cpf_usuario;
+
     FILE *login;
     login = fopen ("usuario.bin", "rb");
-
-    fread(cpf, sizeof(int), 1, login);
-    
-    for(i = 0; i < 1; i++)
-    {
-        printf("%d\n", cpf[i]);
+    fread(&cpf_usuario, sizeof(cpf_usuario), 1, login);
+    fclose(login);
+    if (cpf_entrada == cpf_usuario){
+        printf("Verdade\n");
+    }else{
+        printf("Falso\n");
     }
 
-    for(i = 0; i < 1; i++)
-    {
-        printf("%d\n", senha[i]);
-    }
 }
 
 int main(){
 
-    int cpf [1] = {123};
-    int senha [1] = {321};
+    int cpf = 123;
+    int senha = 321;
     add_usuario(cpf, senha);
-    le_usuario(cpf, senha);
 
     int cpf_entrada;
 
-    printf("Digite seu cpf: ");
+    printf("Digite seu cpf: \n");
     scanf("%d", &cpf_entrada);
-    printf("entrada: %d", cpf_entrada);
+
+    Login_cpf(cpf_entrada);
+
+    printf("entrada: %d\n", cpf_entrada);
 
     return 0;
 }
